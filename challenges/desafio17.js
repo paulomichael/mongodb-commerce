d67422ce18b57f5d24644e38ae757b7fc55bce94
Paulo Michael <paulo.michael@gmail.com>
2022-09-15 11:09:55 -0300

    feat: req17

---
+++ challenges/desafio17.js
@@ -0,0 +1,11 @@
+db.resumoProdutos.insertOne(
+  {
+    franquia: "McDonalds",
+    totalProdutos: db.produtos.find({}).count(),
+  },
+);
+
+db.resumoProdutos.find(
+  {},
+  { franquia: 1, totalProdutos: 1, _id: 0 },
+);
